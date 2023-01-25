21991d1c6c7079b8714d890b8ee7f1aef1626e8f
MateoArteaga <mateomarteaga@gmail.com>
2023-01-24 23:40:19 -0700

    First prep assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    { 
+        Console.Write("What is your first name?");
+        string namefirst = Console.ReadLine();
+        Console.Write("What is your last name?");
+        String namelast = Console.ReadLine();
+        Console.WriteLine($"Your name is {namelast}, {namefirst} {namelast}.");
+        
     }
 }
