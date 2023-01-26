71800e01a8c1b802ab536da5905fe78f4d95bfef
MateoArteaga <mateomarteaga@gmail.com>
2023-01-25 19:53:01 -0700

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        Console.WriteLine($"the magic number is {number}");
+        string response;
+        do
+        {
+            Console.Write("What is your guess?");
+            response = Console.ReadLine();
+            if (int.Parse(response) > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (int.Parse(response) < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        
+        } while (int.Parse(response) != number);
+        Console.WriteLine("you got it!");
     }
 }
