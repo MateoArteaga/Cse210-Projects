d17c172cf5eb0f63e55bc00b07565d242600e144
MateoArteaga <mateomarteaga@gmail.com>
2023-01-25 21:53:15 -0700

    I love Sophie My Beautiful Wife

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name:");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number:");
+            string number = Console.ReadLine();
+            return int.Parse(number);
+        }
+        static int SquareNumber(int number)
+        {
+            double value = number;
+            return Math.Sqrt(number);
+            
+        }
     }
 }
