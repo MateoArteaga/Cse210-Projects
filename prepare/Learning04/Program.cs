d22d51abb755b75405f18de43c418b37d618f538
MateoArteaga <mateomarteaga@gmail.com>
2023-02-15 14:00:48 -0700

    Lerning04 completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Johnny Doctor", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment b = new MathAssignment("Albert Stein", "Relativity", "1.1", "1-25");
+        Console.WriteLine(b.GetSummary());
+        Console.WriteLine(b.GetHomeworkList());
+
+        WritingAssignment c = new WritingAssignment("Thing1", "childrens books", "My Best Friend Thing2");
+        Console.WriteLine(c.GetSummary());
+        Console.WriteLine(c.GetWritingInformation());
     }
 }
